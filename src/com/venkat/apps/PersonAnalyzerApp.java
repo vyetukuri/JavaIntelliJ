91be54457355381e9507cacc4d6521202ad358cd
vyetukuri <vyetukuri@gmail.com>
2020-09-03 22:43:01 -0400

    second commit

--- src/com/venkat/apps/PersonAnalyzerApp.java
+++ src/com/venkat/apps/PersonAnalyzerApp.java
@@ -9,34 +9,6 @@ public class PersonAnalyzerApp {
 
     public static void main(String args[])
     {
-        /*
-
-venkat,cary,nc,usa
-Siva,toronto,ON, canada
-Sai, hyderabad,telengana,India
-suresh,,somethign
-         */
-//        ArrayList<ArrayList<String> > x = new ArrayList<ArrayList<String> >();
-//        x.add(new ArrayList<String>(Arrays.asList("venkat","cary","nc","usa")));
-//        x.add(new ArrayList<String>(Arrays.asList("Siva","toronto","ON","canada")));
-//        x.add(new ArrayList<String>(Arrays.asList("Sai","hyderabad","telengana","India")));
-//        x.add(new ArrayList<String>(Arrays.asList("suresh","","","somethign")));
-//
-//        System.out.println(x);
-//
-//        //create array of employee object
-//        GetPeopleByCity.Person[] obj = new GetPeopleByCity.Person[4] ;
-//        obj[0] = new GetPeopleByCity.Person("Ven1","ABC","","");
-//        System.out.println( obj[0].firstName);
-/*
-        //create & initialize actual employee objects using constructor
-        obj[0] = new GetPeopleByCity.Person("Ven1","ABC","","");
-        obj[1] = new GetPeopleByCity.Person("Ven2","ABC1","","");
-
-        //display the Person object data
-        System.out.println("Employee Object 1:");
-        System.out.println( obj[0]);
-*/
         FileHelper helper = new FileHelper();
         List<String> lines =
                 helper.readFile( "C:\\Users\\vyetukuri\\Selenium\\JavaIntellij\\inputData\\personData" );
